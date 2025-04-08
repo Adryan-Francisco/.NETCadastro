09da1cbe18611c83f78e06da8d005e92a63bb589
Adryan-Francisco <adryanfrancisco62@gmail.com>
2025-04-08 12:49:35 -0300

    Projeto Cadastro em .Net

---
+++ Cadastro/Data/ApplicationDbContext.cs.cs
@@ -0,0 +1,26 @@
+﻿using CrudMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Cadastro.Data
+
+{
+
+    public class ApplicationDbContext : DbContext
+
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+
+            : base(options)
+
+        { }
+
+
+
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+
+}
